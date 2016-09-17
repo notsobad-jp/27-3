f1f9b8c6e0185323845be780fbe5b85de4b0954e
tomomichi <tomomichi.onishi@gmail.com>
2016-09-17 16:18:51 +0900

    add download logging

--- js/application.js
+++ js/application.js
@@ -1,8 +1,59 @@
 $(function(){
+  //TODO: Require Login
+  var reserve_id = 'hogehoge';
+  var map_downloaded = access_downloaded = keybox_downloaded = false;
+
+  //Download
+  $(document).on('click', '#download-map', function(e){
+    e.preventDefault();
+    if(!map_downloaded) {
+      submitDownloadLog({'id': reserve_id, 'type': 'howtogo.jpg'});
+      map_downloaded = true;
+    }
+    var url = '/img/howtogo.jpg';
+    window.open(url, '_blank');
+  });
+  $(document).on('click', '#download-access', function(e){
+    e.preventDefault();
+    if(!access_downloaded) {
+      submitDownloadLog({'id': reserve_id, 'type': 'access.pdf'});
+      access_downloaded = true;
+    }
+    var url = '/img/access.pdf';
+    window.open(url, '_blank');
+  });
+  $(document).on('click', '#download-keybox', function(e){
+    e.preventDefault();
+    if(!keybox_downloaded) {
+      submitDownloadLog({'id': reserve_id, 'type': 'howtoopen.jpg'});
+      keybox_downloaded = true;
+    }
+    var url = '/img/howtoopen.jpg';
+    window.open(url, '_blank');
+  });
+
+  function submitDownloadLog(data) {
+    $.ajax({
+        url: 'https://hooks.zapier.com/hooks/catch/708819/6dm7i2/',
+        method: 'POST',
+        data: data,
+        dataType: 'jsonp',
+        contentType: 'application/json; charset=utf-8',
+        error: function(res, text){
+          console.log('Err', res);
+          console.log(text);
+        },
+        success: function(res){
+          console.log('Success', res);
+        }
+    });
+  }
+
   $(document).on('submit', '#form', function(e){
     e.preventDefault();
     $('#submit').addClass('loading');
     var data = $(this).serialize();
+    data += '&RESERVATION_ID='+reserve_id;
 
     //TODO: Validate data
 
@@ -12,7 +63,6 @@ $(function(){
     }, 1000);
     //submitForm(data);
   });
-
 
   function submitForm(data) {
     $.ajax({
