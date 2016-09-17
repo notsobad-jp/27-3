67c0f543c252c9cc730d74fc1ca2981447f021ef
tomomichi <tomomichi.onishi@gmail.com>
2016-09-17 17:15:19 +0900

    change directory

--- js/application.js
+++ js/application.js
@@ -1,16 +1,22 @@
 $(function(){
-  //TODO: Require Login
-  var reserve_id = 'hogehoge';
-  var map_downloaded = access_downloaded = keybox_downloaded = false;
+  var form_submitted = map_downloaded = access_downloaded = keybox_downloaded = false;
 
-  //Download
+  /************************************************
+  * TODO: Require Login
+  ************************************************/
+  var reserve_id = 'hogehoge';
+
+
+  /************************************************
+  * Data Download
+  ************************************************/
   $(document).on('click', '#download-map', function(e){
     e.preventDefault();
     if(!map_downloaded) {
-      submitDownloadLog({'id': reserve_id, 'type': 'howtogo.jpg'});
+      //submitDownloadLog({'id': reserve_id, 'type': 'howtogo.jpg'});
       map_downloaded = true;
     }
-    var url = '/img/howtogo.jpg';
+    var url = '../../img/howtogo.jpg';
     window.open(url, '_blank');
   });
   $(document).on('click', '#download-access', function(e){
@@ -19,7 +25,7 @@ $(function(){
       submitDownloadLog({'id': reserve_id, 'type': 'access.pdf'});
       access_downloaded = true;
     }
-    var url = '/img/access.pdf';
+    var url = '../../img/access.pdf';
     window.open(url, '_blank');
   });
   $(document).on('click', '#download-keybox', function(e){
@@ -28,60 +34,84 @@ $(function(){
       submitDownloadLog({'id': reserve_id, 'type': 'howtoopen.jpg'});
       keybox_downloaded = true;
     }
-    var url = '/img/howtoopen.jpg';
+    var url = '../../img/howtoopen.jpg';
     window.open(url, '_blank');
   });
 
   function submitDownloadLog(data) {
     $.ajax({
-        url: 'https://hooks.zapier.com/hooks/catch/708819/6dm7i2/',
-        method: 'POST',
-        data: data,
-        dataType: 'jsonp',
-        contentType: 'application/json; charset=utf-8',
-        error: function(res, text){
-          console.log('Err', res);
-          console.log(text);
-        },
-        success: function(res){
-          console.log('Success', res);
-        }
+      url: 'https://hooks.zapier.com/hooks/catch/708819/6dm7i2/',
+      method: 'POST',
+      data: data,
+      dataType: 'jsonp',
+      contentType: 'application/json; charset=utf-8',
+      error: function(res, text){
+        console.log('Err', res);
+        console.log(text);
+      },
+      success: function(res){
+        console.log('Success', res);
+      }
     });
   }
 
+
+  /************************************************
+  * Form Submit
+  ************************************************/
   $(document).on('submit', '#form', function(e){
     e.preventDefault();
+    if(form_submitted) {
+      alert("The form has been submitted already. If you need to fix the information, please reload the page and try again.");
+      return;
+    }
+
     $('#submit').addClass('loading');
     var data = $(this).serialize();
     data += '&RESERVATION_ID='+reserve_id;
 
-    //TODO: Validate data
+    // Validate data
+    var valid = true;
+    $(".required").each(function(index){
+      if($(this).val()==''){
+        valid = false;
+        $(this).parent('.field').addClass('error');
+      }else {
+        $(this).parent('.field').removeClass('error');
+      }
+    });
 
     setTimeout(function(){
       $('#submit').removeClass('loading');
-      $('#modal-success').modal('show');
-    }, 1000);
+      if(valid) {
+        $('#modal-success').modal('show');
+        form_submitted = true;
+      }else {
+        $('#modal-error').modal('show');
+      }
+    }, 10);
     //submitForm(data);
   });
 
   function submitForm(data) {
     $.ajax({
-        url: 'https://hooks.zapier.com/hooks/catch/708819/6dd91x/',
-        method: 'POST',
-        data: data,
-        dataType: 'jsonp',
-        contentType: 'application/json; charset=utf-8',
-        error: function(res, text){
-          $('#modal-error').modal('show');
-          $('#submit').removeClass('loading');
-          console.log('Err', res);
-          console.log(text);
-        },
-        success: function(res){
-          $('#modal-success').modal('show');
-          $('#submit').removeClass('loading');
-          console.log('Success', res);
-        }
+      url: 'https://hooks.zapier.com/hooks/catch/708819/6dd91x/',
+      method: 'POST',
+      data: data,
+      dataType: 'jsonp',
+      contentType: 'application/json; charset=utf-8',
+      error: function(res, text){
+        $('#modal-error').modal('show');
+        $('#submit').removeClass('loading');
+        console.log('Err', res);
+        console.log(text);
+      },
+      success: function(res){
+        form_submitted = true;
+        $('#modal-success').modal('show');
+        $('#submit').removeClass('loading');
+        console.log('Success', res);
+      }
     });
   }
 });
